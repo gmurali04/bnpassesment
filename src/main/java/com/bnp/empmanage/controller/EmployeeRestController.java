a897cf69dd5f5324cec94723745e1e02eb8e424c
gmurali04 <muralicodes04@gmail.com>
2022-06-10 09:28:53 +0530

    csv export

--- src/main/java/com/bnp/empmanage/controller/EmployeeRestController.java
+++ src/main/java/com/bnp/empmanage/controller/EmployeeRestController.java
@@ -4,7 +4,15 @@ import com.bnp.empmanage.entity.Employee;
 import com.bnp.empmanage.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -63,4 +71,30 @@ public class EmployeeRestController {
         employeeService.deleteById(employeeId);
         return "Deleted Employee id - " + employeeId;
     }
-}
+
+    @GetMapping("/employees/export")
+    public void exportToCSV(HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".csv";
+        response.setHeader(headerKey, headerValue);
+
+        List<Employee> listUsers = employeeService.findAll();
+
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeader = {"User ID", "First Name", "Last Name", "Status"};
+        String[] nameMapping = {"uid", "firstName", "lastName", "status"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (Employee user : listUsers) {
+            csvWriter.write(user, nameMapping);
+        }
+
+        csvWriter.close();
+    }
+
+    }
